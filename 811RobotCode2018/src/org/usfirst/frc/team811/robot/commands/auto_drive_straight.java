22ffda054984fdd2c0b740c2d95cb0b97f3c24fe
cardinals <cardinals@lancabot>
2018-02-19 16:18:47 -0500

    pjfpwfjwefjoiewf

---
+++ 811RobotCode2018/src/org/usfirst/frc/team811/robot/commands/auto_drive_straight.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team811.robot.commands;
+
+import org.usfirst.frc.team811.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class auto_drive_straight extends Command {
+
+    public auto_drive_straight() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.motionProfile);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.motionProfile.configureFollower();
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.motionProfile.followTrajectory(false);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return Robot.motionProfile.isFinished();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.motionProfile.reset();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
