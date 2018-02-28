1bfdfd314a6d6aff3ce95cce4d04559724f7641e
cardinals <cardinals@lancabot.lan>
2018-02-28 17:41:05 -0500

    hi

--- 811RobotCode2018/src/org/usfirst/frc/team811/robot/Robot.java
+++ 811RobotCode2018/src/org/usfirst/frc/team811/robot/Robot.java
@@ -150,8 +150,8 @@ public class Robot extends TimedRobot implements Constants {
 		Scheduler.getInstance().run();
 		fourBar.encoderValue();
 		SmartDashboard.putNumber("Pid Output", fourBar.pidGet());
-		double setpoint = SmartDashboard.getNumber("PID Setpoint", 0);
-		fourBar.setPostion(setpoint);
+		//double setpoint = SmartDashboard.getNumber("PID Setpoint", 0);
+		//fourBar.setPostion(setpoint);
 		SmartDashboard.putNumber("Left Drive", RobotMap.drivefrontleft.getSelectedSensorPosition(0));
 		SmartDashboard.putNumber("Right Drive", RobotMap.drivefrontright.getSelectedSensorPosition(0));
 		SmartDashboard.putNumber("Gyro", RobotMap.ahrs.getYaw());
